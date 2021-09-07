373de04f7b6591e5c5ec958834a7ffcd837379b7
MSetiawan2005 <ms0132005@gmail.com>
2021-09-07 18:03:59 +0700

    Update BallControl.cs

--- Assets/BallControl.cs
+++ Assets/BallControl.cs
@@ -36,11 +36,11 @@ public class BallControl : MonoBehaviour
         if (randomDirection < 1.0f)
         {
             // Gunakan gaya untuk menggerakkan bola ini.
-            rigidBody2D.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+            rigidBody2D.AddForce(new Vector2(-xInitialForce, yInitialForce));
         }
         else
         {
-            rigidBody2D.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+            rigidBody2D.AddForce(new Vector2(xInitialForce, yInitialForce));
         }
 
     }
